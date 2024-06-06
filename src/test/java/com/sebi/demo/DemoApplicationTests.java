643d72d26d67e4103d8ff5b104c094543c3ba9f0
OprescuSebastian <sebi.oprescu@yahoo.com>
2024-06-06 20:13:00 +0300

    demo aplication!

---
+++ src/test/java/com/sebi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
